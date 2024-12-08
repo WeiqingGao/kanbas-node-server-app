d25bcdff30cd2a5b0f55fb88a48e38689cab660a
Weiqing Gao <wkinggao@gmail.com>
2024-12-08 16:36:35 -0500

    fix people table not show

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -1,15 +1,17 @@
+//import Database from "../Database/index.js";
 import model from "./model.js";
+
 
 export function findAllCourses() {
     return model.find();
-}
 
+}
 export function findCoursesForEnrolledUser(userId) {
     const { courses, enrollments } = Database;
     const enrolledCourses = courses.filter((course) =>
-      enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
+        enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
     return enrolledCourses;
-  }
+}
 
 export function createCourse(course) {
     delete course._id;
@@ -19,17 +21,14 @@ export function createCourse(course) {
 export function deleteCourse(courseId) {
     return model.deleteOne({ _id: courseId });
 }
-  
+
 export function updateCourse(courseId, courseUpdates) {
     return model.updateOne({ _id: courseId }, { $set: courseUpdates });
 }
+
+
 
 export async function findUsersForCourse(courseId) {
     const enrollments = await enrollmentModel.find({ course: courseId }).populate("user");
     return enrollments.map((enrollment) => enrollment.user);
 }
-
-app.post("/api/courses", async (req, res) => {
-    const course = await dao.createCourse(req.body);
-    res.json(course);
-});
